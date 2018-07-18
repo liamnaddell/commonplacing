adf9f8380c1da7d44647e7da09ce35907e972841
17liamnaddell <liamnprg@gmail.com>
2018-07-18 09:19:03 -0400

    add databases

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,29 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+extern crate postgres;
+
+use postgres::{Connection, TlsMode};
 
 use std::io;
 use std::path::{Path, PathBuf};
-
 use rocket::response::NamedFile;
 
 #[get("/")]
 fn index() -> io::Result<NamedFile> {
+    let string = "postgres://postgres:alliesofphotoyodaarestrong@localhost/mydb";
+    println!("{}",string);
+    let conn = Connection::connect(string, TlsMode::None).unwrap();
+    for row in &conn.query("SELECT * FROM webpage", &[&0]).unwrap() {
+        let bar: String = row.get(0);
+        println!("{}",bar)
+    }
     NamedFile::open("static/index.html")
+
 }
+
+
+
 
 #[get("/<file..>")]
 fn files(file: PathBuf) -> Option<NamedFile> {
